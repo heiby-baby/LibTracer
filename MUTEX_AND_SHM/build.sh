c708716db5bd0ba3c0e2db606294c86335d8f3b0
Maxim Kandrushin <maxim123.321.2014.2@yandex.ru>
2025-03-23 17:22:30 +0300

    correct commit

--- MUTEX_AND_SHM/build.sh
+++ MUTEX_AND_SHM/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm -rf build
+mkdir build
+cd build
 
-gcc reset.c -o reset -lrt -pthread
-gcc increment.c -o increment -lrt -pthread
+gcc ../reset.c -o reset -lrt -pthread
+gcc ../increment.c -o increment -lrt -pthread
